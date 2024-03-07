6b6164b141692de671bf19f4c3ef095c6b380996
Ivan_172 <jorge.herrera.g19@gmail.com>
2024-03-07 08:46:05 -0600

    Diseño Carrito2

--- screens/Products.tsx
+++ screens/Products.tsx
@@ -3,9 +3,23 @@ import React, { useState, useEffect } from 'react';
 import { View, ScrollView } from 'react-native';
 import axios from 'axios';
 import ProductComponent from './ProductTS';
+import {
+  Box,
+  VStack,
+  Heading,
+  Input,InputSlot, SearchIcon, InputIcon,
+  InputField,
+  Button,
+  ButtonText,
+  Image,
+  Center,
+  Text, 
+  
+} from '@gluestack-ui/themed';
 
 const CatalogComponent: React.FC = () => {
   const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     // Realizar la solicitud a la API
@@ -20,14 +34,35 @@ const CatalogComponent: React.FC = () => {
       });
   }, []); // La dependencia vacía significa que este efecto se ejecutará solo una vez al montar el componente
 
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
+    <>
+    <Box backgroundColor='black' paddingTop={'$10'} paddingLeft={'$2'} paddingRight={'$2'}>
+    
+        </Box>
     <ScrollView style={{ backgroundColor: 'black'}}>
+      
       <View>
-        {products.map(product => (
+      <Input  
+    borderRadius={'$full'}>
+          <InputSlot bgColor='white' >
+            <InputIcon as={SearchIcon} />
+          </InputSlot>
+          <InputField
+            placeholder="Search..." bgColor='white'
+            onChangeText={(text) => setSearchQuery(text)}
+            value={searchQuery}
+          />
+        </Input>
+        {filteredProducts.map(product => (
           <ProductComponent key={product.id} product={product} />
         ))}
       </View>
     </ScrollView>
+    </>
   );
 };
 
